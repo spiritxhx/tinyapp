90f88c0cab8d059e449493936a0567acdc8eb5ea
spiritxhx <spiritxhx@gmail.com>
2019-07-08 16:50:35 -0400

    add post request in the table

--- express_server.js
+++ express_server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(6);
+};
+
+
 
 app.set('view engine', 'ejs');
 
@@ -11,6 +19,17 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(generateRandomString());
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get('/urls.json', (req, res) => {
