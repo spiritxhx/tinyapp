abd957455533be5550ffa9bdac49686ff40091b9
spiritxhx <spiritxhx@gmail.com>
2019-07-10 10:14:28 -0400

    create the registration page

--- express_server.js
+++ express_server.js
@@ -12,11 +12,25 @@ const generateRandomString = () => {
 };
 
 app.set('view engine', 'ejs');
-
+//--------------data------------------------------
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+//--------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -58,7 +72,6 @@ app.get('/urls', (req, res) => {
     username: req.cookies['name'],
     urls: urlDatabase
   };
-  console.log(req.cookies);
   res.render("urls_index", templateVars);
 });
 
@@ -84,10 +97,19 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+//enter user registration page-------------------
+app.get("/register", (req, res)=> {
+  res.render("/register");
+});
+
+
+//Login--------------------------------------
 app.post("/login", (req, res) => {
   res.cookie('name', req.body.username);
   res.redirect('/urls');
 });
+//---------------------------------------
+
 
 //logout
 app.post("/logout", (req, res) => {
